1c6c45a085eabbd9de6e93b88978398230980b5f
gavinzhou <gavin.zhou@gmail.com>
2018-11-09 21:04:58 +0900

    Added tag

--- models/tag.go
+++ models/tag.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
+
 type Tag struct {
 	Model
 
@@ -7,6 +13,19 @@ type Tag struct {
 	CreatedBy  string `json:"created_by"`
 	ModifiedBy string `json:"modified_by"`
 	State      int    `json:"state"`
+}
+
+func (tag *Tag) BeforeCreate(scope *gorm.Scope) error {
+	scope.SetColumn("CreatedOn", time.Now().Unix())
+	scope.SetColumn("ModifiedOn", time.Now().Unix())
+
+	return nil
+}
+
+func (tag *Tag) BeforeUpdate(scope *gorm.Scope) error {
+	scope.SetColumn("ModifiedOn", time.Now().Unix())
+
+	return nil
 }
 
 func GetTags(pageNum int, pageSize int, maps interface{}) (tags []Tag) {
@@ -19,4 +38,25 @@ func GetTagTotal(maps interface{}) (count int) {
 	db.Model(&Tag{}).Where(maps).Count(&count)
 
 	return
+}
+
+func ExistTagByName(name string) bool {
+	var tag Tag
+	db.Select("id").Where("name = ?", name).First(&tag)
+	return tag.ID > 0
+	// if tag.ID > 0 {
+	// 	return true
+	// }
+
+	// return false
+}
+
+func AddTag(name string, state int, createdBy string) bool {
+	db.Create(&Tag{
+		Name:      name,
+		State:     state,
+		CreatedBy: createdBy,
+	})
+
+	return true
 }
--- routers/api/v1/tag.go
+++ routers/api/v1/tag.go
@@ -1,9 +1,11 @@
 package v1
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/Unknwon/com"
+	"github.com/astaxie/beego/validation"
 	"github.com/gavinzhou/hello-gin/models"
 	"github.com/gavinzhou/hello-gin/pkg/e"
 	"github.com/gavinzhou/hello-gin/pkg/setting"
@@ -40,6 +42,36 @@ func GetTags(c *gin.Context) {
 }
 
 func AddTag(c *gin.Context) {
+	name := c.Query("name")
+	state := com.StrTo(c.DefaultQuery("state", "0")).MustInt()
+	createdBy := c.Query("created_by")
+
+	valid := validation.Validation{}
+	valid.Required(name, "name").Message("name is not null")
+	valid.MaxSize(name, 100, "name").Message("name max length is 100")
+	valid.Required(createdBy, "created_by").Message("Auth is not null")
+	valid.MaxSize(createdBy, 100, "created_by").Message("auth name max length is 100")
+	valid.Range(state, 0, 1, "state").Message("state only 0 or 1")
+
+	code := e.INVALID_PARAMS
+	if !valid.HasErrors() {
+		if !models.ExistTagByName(name) {
+			code = e.SUCCESS
+			models.AddTag(name, state, createdBy)
+		} else {
+			code = e.ERROR_EXIST_TAG
+		}
+	} else {
+		for _, err := range valid.Errors {
+			log.Fatalln(err.Key, err.Message)
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": make(map[string]string),
+	})
 }
 
 func EditTag(c *gin.Context) {
